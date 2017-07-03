5f959e20300bfc3d1be550d0fd91ad269c7a18c3
Petro Knysh <peteknysh@gmail.com>
2017-07-04 01:31:19 +0300

    Last day commit

--- brief/brief.Data/AuthorRepository.cs
+++ brief/brief.Data/AuthorRepository.cs
@@ -12,13 +12,9 @@
         public AuthorRepository(string connectionString) : base(connectionString) {}
 
         public async Task<Author> GetAuthor(Guid id)
-        {
-            var author = await Connection.QueryFirstAsync<Author>("select Id, AuthorFirstName, AuthorSecondName, AuthorLastName " +
+            => await Connection.QueryFirstAsync<Author>("select Id, AuthorFirstName, AuthorSecondName, AuthorLastName " +
                                                                   "from dbo.authors where Id = @authorId", new { authorId = id });
-
-            return author;
-        }
-
+        
         public async Task<bool> CheckAuthorForUniqueness(Author author)
         {
             var existingCount = (await Connection.QueryAsync<int>("select count(*) from dbo.authors where AuthorFirstName = @authorFirstName and " +
@@ -29,27 +25,46 @@
                                                                       authorSecondName = author.AuthorSecondName,
                                                                       authorLastName = author.AuthorLastName
                                                                   })).Single();
-
             if (existingCount != 0)
             {
                 return false;
             }
+
             return true;
         }
 
-        public Task<Guid> CreateAuthor(Author author)
+        public async Task<Guid> CreateAuthor(Author author)
         {
-            throw new NotImplementedException();
+            await Connection.ExecuteAsync("insert into dbo.authors (Id, AuthorFirstName, AuthorSecondName, AuthorLastName)" +
+                                          " values (@id, @authorFirstName, @authorSecondName, @authorLastName)", 
+                                          new
+                                          {
+                                              id = author.Id,
+                                              authorFirstName = author.AuthorFirstName,
+                                              authorSecondName = author.AuthorSecondName,
+                                              authorLastName = author.AuthorLastName
+                                          });
+            return author.Id;
         }
 
-        public Task<Guid> UpdateAuthor(Author author)
+        public async Task<Guid> UpdateAuthor(Author author)
         {
-            throw new NotImplementedException();
+            await Connection.ExecuteAsync("update dbo.authors set AuthorFirstName = @authorFirstName," +
+                                          " AuthorSecondName = @authorSecondName," +
+                                          " AuthorLastName = @authorLastName) where Id = @id",
+                new
+                {
+                    id = author.Id,
+                    authorFirstName = author.AuthorFirstName,
+                    authorSecondName = author.AuthorSecondName,
+                    authorLastName = author.AuthorLastName
+                });
+
+            return author.Id;
         }
 
-        public Task RemoveAuthor(Author author)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task RemoveAuthor(Author author)
+            => await Connection.ExecuteAsync("delete from dbo.authors where Id = @authorId",
+                new {authorId = author.Id});
     }
 }
