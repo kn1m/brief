a2251f0536289dbe6433eb33b2e716c9377da10a
Petro Knysh <peteknysh@gmail.com>
2017-07-09 02:26:10 +0300

    Migrated to OWIN startup

--- brief/brief.Controllers/Constraints/LowercaseRouteConstraint.cs
+++ brief/brief.Controllers/Constraints/LowercaseRouteConstraint.cs
@@ -1,14 +1,26 @@
 ﻿namespace brief.Controllers.Constraints
 {
     using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
     using System.Web;
+    using System.Web.Http.Routing;
     using System.Web.Routing;
 
-    public class LowercaseRouteConstraint : IRouteConstraint
+    public class LowercaseRouteConstraint : IRouteConstraint, IHttpRouteConstraint
     {
+        //TODO: remove?
         public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
         {
             var path = httpContext.Request.Url?.AbsolutePath;
+            return path.Equals(path.ToLowerInvariant(), StringComparison.InvariantCulture);
+        }
+
+        //OWIN constraint
+        public bool Match(HttpRequestMessage request, IHttpRoute route, string parameterName, IDictionary<string, object> values,
+            HttpRouteDirection routeDirection)
+        {
+            var path = request.RequestUri.AbsolutePath;
             return path.Equals(path.ToLowerInvariant(), StringComparison.InvariantCulture);
         }
     }
--- brief/brief/Startup.cs
+++ brief/brief/Startup.cs
@@ -1,18 +1,21 @@
 ﻿namespace brief
 {
+    using System.Web.Http;
     using Autofac;
     using Autofac.Integration.WebApi;
-    using System.Web.Http;
     using AutoMapper;
     using Library.Entities.Profiles;
     using Modules;
+    using Owin;
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class Startup
     {
-        protected void Application_Start()
+        public void Configuration(IAppBuilder app)
         {
-            var builder = new ContainerBuilder();
+            var config = new HttpConfiguration();
 
+            var builder = new ContainerBuilder();
+            
             builder.Register(ctx => new MapperConfiguration(cfg =>
             {
                 cfg.AddProfiles(typeof(BookProfile).Assembly);
@@ -20,17 +23,25 @@
 
             builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>();
 
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            var config = GlobalConfiguration.Configuration;
-
             builder.RegisterModule(new ServicesModule());
             builder.RegisterModule(new DataModule());
             builder.RegisterModule(new ControllersModule());
 
             // Set the dependency resolver to be Autofac.
+            //var container = builder.Build();
+            //config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
             var container = builder.Build();
+
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            // Register the Autofac middleware FIRST. This also adds
+            // Autofac-injected middleware registered with the container.
+
+            WebApiConfig.Register(config);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
         }
     }
 }
