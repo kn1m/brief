9ae6ae029f1bbeee8c56a05d5edd865aa4a220ca
Petro Knysh <peteknysh@gmail.com>
2017-10-27 00:24:12 +0300

    started adding mappings for new entities

--- brief/brief.Data/Maps/NoteMap.cs
+++ brief/brief.Data/Maps/NoteMap.cs
@@ -1,6 +1,25 @@
 ﻿namespace brief.Data.Maps
 {
-    class NoteMap
+    using System.Data.Entity.ModelConfiguration;
+    using Library.Entities;
+
+    public class NoteMap : EntityTypeConfiguration<Note>
     {
+        public NoteMap()
+        {
+            ToTable("notes");
+
+            HasKey(n => n.Id);
+
+            Property(n => n.NoteTitle)
+                .HasMaxLength(300);
+
+            Property(n => n.NoteText)
+                .HasMaxLength(4000)
+                .IsRequired();
+
+            Property(n => n.NoteType)
+                .IsRequired();
+        }
     }
 }
