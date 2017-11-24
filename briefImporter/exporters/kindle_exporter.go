f2e503421a58326865b91dae43c04980cfc9b69e
Petro Knysh <peteknysh@gmail.com>
2017-11-24 12:52:07 +0200

    some improvements

--- briefImporter/exporters/kindle_exporter.go
+++ briefImporter/exporters/kindle_exporter.go
@@ -1,53 +1,56 @@
 package exporters
 
 import (
-	"errors"
-	"strings"
-	"regexp"
 	"brief/briefImporter/common"
-	"strconv"
+	"errors"
 	"log"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-const titleGroupName          = "title"
-const alttitleGroupName       = "alttitle"
-const authorGroupName         = "author"
-const pageGroupName           = "page"
-const firstLocationGroupName  = "location"
-const secondLocationGroupName = "slocation"
-const createdOnDateGroupName  = "createdondate"
-const createdOnTimeGroupName  = "createdontime"
-const noteDataGroupName       = "notedata"
-const recordTypeGroupName     = "recordtype"
+const (
+	titleGroupName          = "title"
+	alttitleGroupName       = "alttitle"
+	authorGroupName         = "author"
+	pageGroupName           = "page"
+	firstLocationGroupName  = "location"
+	secondLocationGroupName = "slocation"
+	createdOnDateGroupName  = "createdondate"
+	createdOnTimeGroupName  = "createdontime"
+	noteDataGroupName       = "notedata"
+	recordTypeGroupName     = "recordtype"
+)
 
-var recordTypesToSkip  = []string{"Highlight", "Bookmark"}
+type (
+	NoteRecord struct {
+		BookTitle        string   `json:"book_title"`
+		BookOriginalName string   `json:"book_original_name"`
+		BookAuthor       []Author `json:"authors"`
+		FirstPage        int      `json:"first_page"`
+		SecondPage       int      `json:"second_page"`
+		FirstLocation    int      `json:"first_location"`
+		SecondLocation   int      `json:"second_location"`
+		CreatedOn        string   `json:"created_on"`
+		NoteTitle        string   `json:"note_title"`
+		NoteText         string   `json:"note_text"`
+	}
 
-type NoteRecord struct
-{
-	BookTitle string        `json:"book_title"`
-	BookOriginalName string `json:"book_original_name"`
-	BookAuthor []Author     `json:"authors"`
-	FirstPage int 			`json:"first_page"`
-	SecondPage int          `json:"second_page"`
-	FirstLocation int       `json:"first_location"`
-	SecondLocation int      `json:"second_location"`
-	CreatedOn string        `json:"created_on"`
-	NoteTitle string        `json:"note_title"`
-	NoteText string         `json:"note_text"`
-}
+	Author struct {
+		FirstName     string
+		SecondaryName string
+		Surname       string
+	}
 
-type Author struct {
-	FirstName string
-	SecondaryName string
-	Surname string
-}
+	NoteData struct {
+		titleNoteData map[string]string
+		noteData      map[string]string
+	}
+)
 
-type NoteData struct {
-	titleNoteData map[string]string
-	noteData map[string]string
-}
+var recordTypesToSkip = []string{"Highlight", "Bookmark"}
 
-func GetNotes(path string) ([]NoteRecord, error){
+func GetNotes(path string) ([]NoteRecord, error) {
 
 	log.Printf("Processing file %s:\n", path)
 
@@ -69,23 +72,23 @@ func GetNotes(path string) ([]NoteRecord, error){
 
 	str := string(file_data)
 
-	recordRegexp := regexp.MustCompile(`(g?)(i?)(?P<`+ titleGroupName +`>[\wА-Яа-яіїєґ'#\-*:*\s*\.*\,*]+)\s` +
-		`(?P<`+ alttitleGroupName +`>\({1}[\wА-Яа-яіїєґ\s*\.*\,*]+\){1})?\s?` +
-		`(\({1}(?P<`+ authorGroupName +`>[\wА-Яа-яіїєґ\;*\s*\.*\,*]+)\){1}){1}` +
-		`[\r\n]*-\sYour\s(?P<`+ recordTypeGroupName +`>(Note|Highlight|Bookmark))\son\s` +
-		`(page\s(?P<`+ pageGroupName +`>[\d]+)\s\|\s)?` +
-		`Location\s(?P<`+ firstLocationGroupName +`>[\d]+)\-?`+
-		`(?P<`+ secondLocationGroupName +`>[\d]+)?\s\|\sAdded\son\s` +
-		`[\w]+\,{1}\s(?P<`+ createdOnDateGroupName +`>[\w]+\s[\d]+\,\s\d{4})`+
-		`\s(?P<`+ createdOnTimeGroupName +`>\d{1,2}:\d{2}:\d{2}\s(AM|PM))` +
-		`[\r\n]*(?P<`+ noteDataGroupName +`>[\S\s]+)[\r\n]*`)
+	recordRegexp := regexp.MustCompile(`(g?)(i?)(?P<` + titleGroupName + `>[\wА-Яа-яіїєґ'#\-*:*\s*\.*\,*]+)\s` +
+		`(?P<` + alttitleGroupName + `>\({1}[\wА-Яа-яіїєґ\s*\.*\,*]+\){1})?\s?` +
+		`(\({1}(?P<` + authorGroupName + `>[\wА-Яа-яіїєґ\;*\s*\.*\,*]+)\){1}){1}` +
+		`[\r\n]*-\sYour\s(?P<` + recordTypeGroupName + `>(Note|Highlight|Bookmark))\son\s` +
+		`(page\s(?P<` + pageGroupName + `>[\d]+)\s\|\s)?` +
+		`Location\s(?P<` + firstLocationGroupName + `>[\d]+)\-?` +
+		`(?P<` + secondLocationGroupName + `>[\d]+)?\s\|\sAdded\son\s` +
+		`[\w]+\,{1}\s(?P<` + createdOnDateGroupName + `>[\w]+\s[\d]+\,\s\d{4})` +
+		`\s(?P<` + createdOnTimeGroupName + `>\d{1,2}:\d{2}:\d{2}\s(AM|PM))` +
+		`[\r\n]*(?P<` + noteDataGroupName + `>[\S\s]+)[\r\n]*`)
 
 	split := regexp.MustCompile("={10}[\r\n]*").Split(str, -1)
 
 	var notes []NoteRecord
 
 	i := 0
-	for i < len(split) - 1 {
+	for i < len(split)-1 {
 		var noteData NoteData
 		titleGroup := common.GetGroupsData(recordRegexp, split[i])
 
@@ -100,7 +103,7 @@ func GetNotes(path string) ([]NoteRecord, error){
 		note := &NoteRecord{}
 
 		checkNoteFiled(noteData, note.checkTitle, note.checkAltTitle,
-			           note.checkAuthor, note.checkPage, note.checkLocations, note.checkDateAndTime, note.checkNoteTitleAndText)
+			note.checkAuthor, note.checkPage, note.checkLocations, note.checkDateAndTime, note.checkNoteTitleAndText)
 		notes = append(notes, *note)
 
 		i += 2
@@ -126,7 +129,7 @@ func (note *NoteRecord) checkTitle(data NoteData) (*NoteRecord, error) {
 }
 
 func (note *NoteRecord) checkAltTitle(data NoteData) (*NoteRecord, error) {
-	if baseNoteFieldCheck(data, alttitleGroupName, true){
+	if baseNoteFieldCheck(data, alttitleGroupName, true) {
 		note.BookOriginalName = data.titleNoteData[alttitleGroupName]
 		return note, nil
 	}
@@ -134,7 +137,7 @@ func (note *NoteRecord) checkAltTitle(data NoteData) (*NoteRecord, error) {
 }
 
 func (note *NoteRecord) checkAuthor(data NoteData) (*NoteRecord, error) {
-	if baseNoteFieldCheck(data, authorGroupName, false){
+	if baseNoteFieldCheck(data, authorGroupName, false) {
 		authors := strings.Split(data.titleNoteData[authorGroupName], ";")
 		if len(authors) > 1 {
 			for i := range authors {
@@ -243,17 +246,17 @@ func baseNoteFieldCheck(data NoteData, groupName string, isOptional bool) bool {
 func handleAuthor(authorData []string) (*Author, error) {
 	author := &Author{}
 	switch len(authorData) {
-		case 1:
-			author.FirstName = authorData[0]
-		case 2:
-			author.FirstName = authorData[0]
-			author.Surname = authorData[1]
-		case 3:
-			author.FirstName = authorData[0]
-			author.SecondaryName = authorData[1]
-			author.Surname = authorData[2]
-		default:
-			return &Author{FirstName:"Authors unknown"}, nil
+	case 1:
+		author.FirstName = authorData[0]
+	case 2:
+		author.FirstName = authorData[0]
+		author.Surname = authorData[1]
+	case 3:
+		author.FirstName = authorData[0]
+		author.SecondaryName = authorData[1]
+		author.Surname = authorData[2]
+	default:
+		return &Author{FirstName: "Authors unknown"}, nil
 	}
 	return author, nil
 }
